6678c0febe4523aca41d7a4ab6879aa78694b5f7
billybonks <sebastienstettler@gmail.com>
2019-04-04 01:52:42 +0800

    prevent collision

--- app/models/board.js
+++ app/models/board.js
@@ -24,24 +24,47 @@ export default class Boat {
     this.board[rowIndex][colIndex] = true;
   }
 
-  insertBoat(boat){
+  insertBoat(boat) {
+    let inserted = this.attemptInsert(boat);
+    while(!inserted){
+      debugger
+      console.log('collided')
+      inserted = this.attemptInsert(boat);
+    }
+  }
+
+  attemptInsert(boat) {
     let c = null;
     let r = null;
     if(boat.direction === VERTICAL) {
-      c = Math.abs(Math.floor(Math.random() *  10 - boat.length));
-      r = Math.abs(Math.floor(Math.random() *  10));
+      c = Math.abs(Math.floor(Math.random() *  this.size - boat.length));
+      r = Math.abs(Math.floor(Math.random() *  this.size));
+      for(let i = 0; i < boat.length; i++) {
+        //dry run
+        if(this.board[r][c+i] === 2){
+          return false;
+        }
+      }
+      //insert
       for(let i = 0; i < boat.length; i++) {
         this.board[r][c+i] = 2;
       }
     }
 
     if(boat.direction === HORIZONTAL) {
-      c = Math.abs(Math.floor(Math.random() *  10));
-      r = Math.abs(Math.floor(Math.random() *  10 - boat.length));
+      c = Math.abs(Math.floor(Math.random() *  this.size));
+      r = Math.abs(Math.floor(Math.random() *  this.size - boat.length));
+      //dry run
+      for(let i = 0; i < boat.length; i++) {
+        if(this.board[r+i][c] === 2){
+          return false;
+        }
+      }
+      //insert
       for(let i = 0; i < boat.length; i++) {
         this.board[r+i][c] = 2;
       }
     }
-
+    return true
   }
 }
